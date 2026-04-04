5824e330ead99449039649ae290dbc49d99c50e7
Zafer <zaferkarci@hotmail.com>
2026-04-04 12:25:03 +0300

    Kod 1.3: LDP Logaritmik Dinamik Puanlama ve 5 Seviyeli Zorluk entegre edildi

--- index.js
+++ index.js
@@ -116,24 +116,28 @@ app.post('/cevap', async (req, res) => {
 
             if (dogruMu) {
                 const dersSorulari = await Soru.find({ ders: s.ders, cozulmeSayisi: { $gt: 0 } });
-                let kazanilanPuan = 10;
+                let Z_katsayi = 3; 
                 if (dersSorulari.length > 1) {
                     const basariOranlari = dersSorulari.map(soru => (soru.dogruSayisi / soru.cozulmeSayisi) * 100);
                     const sureler = dersSorulari.map(soru => soru.ortalamaSure || 0);
                     const mBasari = basariOranlari.reduce((a, b) => a + b, 0) / basariOranlari.length;
                     const sBasari = Math.sqrt(basariOranlari.reduce((a, b) => a + Math.pow(b - mBasari, 2), 0) / basariOranlari.length) || 1;
                     const mSure = sureler.reduce((a, b) => a + b, 0) / sureler.length;
-                    const sSure = Math.sqrt(sureler.reduce((a, b) => a + Math.pow(b - mSure, 2) , 0) / sureler.length) || 1;
+                    const sSure = Math.sqrt(sureler.reduce((a, b) => a + Math.pow(b - mSure, 2), 0) / sureler.length) || 1;
                     const zB = (((s.dogruSayisi / s.cozulmeSayisi) * 100) - mBasari) / sBasari;
                     const zS = (s.ortalamaSure - mSure) / sSure;
                     const zorluk = (zS * 0.5) - (zB * 0.5);
-                    if (zorluk < -1.2) kazanilanPuan = 5;
-                    else if (zorluk < -0.5) kazanilanPuan = 8;
-                    else if (zorluk < 0.5) kazanilanPuan = 12;
-                    else if (zorluk < 1.2) kazanilanPuan = 16;
-                    else kazanilanPuan = 22;
+                    if (zorluk < -1.2) Z_katsayi = 1;
+                    else if (zorluk < -0.5) Z_katsayi = 2;
+                    else if (zorluk < 0.5) Z_katsayi = 3;
+                    else if (zorluk < 1.2) Z_katsayi = 4;
+                    else Z_katsayi = 5;
                 }
-                k.puan += kazanilanPuan;
+                const T_ref = s.ortalamaSure || 60;
+                const T_ogr = Math.max(parseInt(gecenSure), 1);
+                const GE = 0.05;
+                const kazanilanPuan = Math.round((Z_katsayi * T_ref * Math.log2(1 + (T_ref / T_ogr))) * GE) || 1;
+                k.puan += Math.max(kazanilanPuan, 1);
             }
             k.toplamSure += parseInt(gecenSure) || 0;
             k.cozumSureleri.push({ soruId: soruId, sure: parseInt(gecenSure) || 0 });
