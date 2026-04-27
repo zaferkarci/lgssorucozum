2a7863fd7acd42d4840d4d3225e4c045b1f35a36
Zafer <zaferkarci@hotmail.com>
2026-04-27 16:01:02 +0300

    feat: giriş sayfasının sol panelini kayıt sayfası bilgilendirmesiyle değiştir

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// --- LGS HAZIRLIK PLATFORMU - VERSİYON 4.0.6 (Modüler Yapı) ---
+// --- LGS HAZIRLIK PLATFORMU - VERSİYON 4.0.7 (Modüler Yapı) ---
 
 const mongoose = require('mongoose');
 const express = require('express');
