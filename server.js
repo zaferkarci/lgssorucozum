99787bcd4ceb9ee0b21fb218ec1222bec364d422
Zafer <zaferkarci@hotmail.com>
2026-04-30 10:32:37 +0300

    fix: sik metni required kaldirildi, gorsel tek basina yeterli

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// --- LGS HAZIRLIK PLATFORMU - VERSİYON 4.0.28 (Modüler Yapı) ---
+// --- LGS HAZIRLIK PLATFORMU - VERSİYON 4.0.29 (Modüler Yapı) ---
 
 const mongoose = require('mongoose');
 const express = require('express');
