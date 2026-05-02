ace09ddd9212a1136dde0e472253fdec01b7e08a
Zafer <zaferkarci@hotmail.com>
2026-05-02 22:35:26 +0300

    feat: konu bazli basari 4 renkli esik (kirmizi/turuncu/sari/yesil)

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// --- LGS HAZIRLIK PLATFORMU - VERSİYON 4.1.14 (Modüler Yapı) ---
+// --- LGS HAZIRLIK PLATFORMU - VERSİYON 4.1.15 (Modüler Yapı) ---
 
 const mongoose = require('mongoose');
 const express = require('express');
